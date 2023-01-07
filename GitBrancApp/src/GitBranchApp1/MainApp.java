6adbba1dc3e8ad10bd5cb8af80433f5058e24d09
GavinLi <fps4735978@gmail.com>
2023-01-07 15:17:24 +0800

    aaa

--- GitBrancApp/src/GitBranchApp1/MainApp.java
+++ GitBrancApp/src/GitBranchApp1/MainApp.java
@@ -4,30 +4,18 @@ public class MainApp {
 	public static void main(String[]args) {
 		//�ժ�
 		System.out.println("1st Version");
-<<<<<<< HEAD
-<<<<<<< HEAD
-		//�ժ�(�s�W��)
-		Math cMath = new Math();
-		System.out.println();
-		System.out.println(cMath.add(10, 20));
 	}
 }
 
 class Math{
 	int add(int a, int b) {
 		return a + b;
-=======
-=======
->>>>>>> 3cc3e70e88f51d11ea4c3718a8f2d5176aa1e91c
 		
 		
 //		  /\＿/\                     /\  /\
 //		 (   •ω•)                   ( •ω•  )
 //		 />☕️                            ☕️<\
 //		 窩們                           窩們
-<<<<<<< HEAD
->>>>>>> branch 'master' of https://github.com/hiro840610/GitBranch.git
-=======
->>>>>>> 3cc3e70e88f51d11ea4c3718a8f2d5176aa1e91c
+
 	}
 }
