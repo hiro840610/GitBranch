e8097edc8972038b934aa29f9185ea3f1fb607c2
Bruce <Bruce03080308@gmail.com>
2023-01-07 14:12:20 +0800

    Add Math Class

--- GitBrancApp/src/GitBranchApp1/MainApp.java
+++ GitBrancApp/src/GitBranchApp1/MainApp.java
@@ -4,5 +4,15 @@ public class MainApp {
 	public static void main(String[]args) {
 		//組長
 		System.out.println("1st Version");
+		//組長(新增的)
+		Math cMath = new Math();
+		System.out.println();
+		System.out.println(cMath.add(10, 20));
+	}
+}
+
+class Math{
+	int add(int a, int b) {
+		return a + b;
 	}
 }
