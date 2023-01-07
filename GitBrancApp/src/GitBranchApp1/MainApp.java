3cc3e70e88f51d11ea4c3718a8f2d5176aa1e91c
LAPTOP-PATF7J21\Tibame_T14 <zxchuahua2417@gmail.com>
2023-01-07 15:05:41 +0800

    test123

--- GitBrancApp/src/GitBranchApp1/MainApp.java
+++ GitBrancApp/src/GitBranchApp1/MainApp.java
@@ -2,7 +2,13 @@ package GitBranchApp1;
 
 public class MainApp {
 	public static void main(String[]args) {
-		//�ժ�
+		//�ժ�
 		System.out.println("1st Version");
+		
+		
+//		  /\＿/\                     /\  /\
+//		 (   •ω•)                   ( •ω•  )
+//		 />☕️                            ☕️<\
+//		 窩們                           窩們
 	}
 }
